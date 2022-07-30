eeb45b2a63d3c8a9f7d0a424e3f526eb76e8218f
shiran <listgus@gmail.com>
2022-07-30 16:36:53 +0300

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WelcomeComponent} from "./components/welcome/welcome.component";
+import {MainComponent} from "./components/main/main.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full'
+  },
+  {
+    path: 'welcome',
+    component: WelcomeComponent
+  },
+  {
+    path: 'main',
+    component: MainComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './components/book/book.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { MainComponent } from './components/main/main.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookListComponent,
+    SearchBarComponent,
+    BooksListComponent,
+    WelcomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
